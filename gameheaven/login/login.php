1be05d6d6361f6210e27571d71ede942298b637f
andreassss02 <133357892+andreeaass02@users.noreply.github.com>
2024-05-29 18:01:53 +0200

    Add files via upload

--- gameheaven/login/login.php
+++ gameheaven/login/login.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/style.css">
-    <title>Document</title>
+    <title>Inicio de Sesión</title>
 </head>
 <body>
-<img src="../images/Captura_de_pantalla_2024-04-09_113755-removebg-preview.png" alt="logo">
+    <img src="../images/Captura_de_pantalla_2024-04-09_113755-removebg-preview.png" alt="logo">
     <div class="wrapper">
         <h1>Inicio de Sesión</h1>
         <form method="post" action="<?=htmlspecialchars($_SERVER['PHP_SELF'])?>">
@@ -20,33 +20,33 @@
             <input type="submit" value="Acceder">
         </form>
         <div class="miembro">
-            ¿Todavia no eres miembro? <a href="../registro.php">Registrate</a>
+            ¿Todavía no eres miembro? <a href="../registro.php">Regístrate</a>
         </div>
     </div>
     
     <?php
-require_once("usuarios.php");
-if($_SERVER["REQUEST_METHOD"]=="POST"){
-    if(isset($_POST["usuario"]) && isset($_POST["contra"])){
-        $usuario = trim(htmlspecialchars($_POST["usuario"]));
-        $contra = trim(htmlspecialchars($_POST["contra"]));
+    require_once("usuarios.php");
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (isset($_POST["usuario"]) && isset($_POST["contra"])) {
+            $usuario = trim(htmlspecialchars($_POST["usuario"]));
+            $contra = trim(htmlspecialchars($_POST["contra"]));
 
-        if(empty($usuario) || empty($contra)){
-            $errors [] = "Debes introducir todos los campos";
-        }
-
-        if(!empty($errors)){
-            foreach($errors as $e){
-                echo $e;
+            if (empty($usuario) || empty($contra)) {
+                $errors [] = "Debes introducir todos los campos";
             }
-        }else{
-            $usuarioObj = new Usuario();
-            $usuarioObj->verificar($usuario, $contra);
+
+            if (!empty($errors)) {
+                foreach ($errors as $e) {
+                    echo $e;
+                }
+            } else {
+                $usuarioObj = new Usuario();
+                $usuarioObj->verificar($usuario, $contra);
+            }
+        } else {
+            echo "Error: Los índices 'usuario' y 'contra' no existen en el array \$_POST.";
         }
-    } else {
-        echo "Error: Los índices 'usuario' y 'contra' no existen en el array \$_POST.";
     }
-}
-?>
+    ?>
 </body>
 </html>
